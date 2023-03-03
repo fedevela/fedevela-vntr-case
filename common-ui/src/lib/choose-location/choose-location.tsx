42366fb25c5b7270f2ff6beab1903d356f83c160
Federico Vela <fedevela@gmail.com>
2023-03-03 16:37:23 -0500

    choose place with lat long

--- common-ui/src/lib/choose-location/choose-location.tsx
+++ common-ui/src/lib/choose-location/choose-location.tsx
@@ -8,19 +8,25 @@ export function ChooseLocation(props: ChooseLocationProps) {
   const autoCompleteRef = useRef(null);
   const inputRef = useRef(null);
   const options = {
-   fields: ["address_components", "geometry", "icon", "name"],
-   types: ["establishment"]
+    fields: ['address_components', 'geometry', 'icon', 'name'],
+    types: ['establishment'],
   };
+
   useEffect(() => {
-   autoCompleteRef.current = new window.google.maps.places.Autocomplete(
-    inputRef.current,
-    options
-   );
+    autoCompleteRef.current = new window.google.maps.places.Autocomplete(
+      inputRef.current,
+      options
+    );
+    autoCompleteRef.current.addListener('place_changed', async function () {
+      const place = await autoCompleteRef.current.getPlace();
+      console.log(`(${place.geometry.location.lat()},${place.geometry.location.lng()})`);
+    });
   }, []);
+
   return (
     <div className={styles['container']}>
-     <label>enter address :</label>
-     <input ref={inputRef} />
+      <label>enter address :</label>
+      <input ref={inputRef} />
     </div>
   );
 }
